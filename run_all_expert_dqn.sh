4e0aa156d987d8a0d164e614f215b4d2b834e8f5
Xiru Zhu <xiruzhu@beluga3.int.ets1.calculquebec.ca>
2019-09-18 16:10:39 -0400

    latest

--- run_all_expert_dqn.sh
+++ run_all_expert_dqn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$special=$1
+special=$1
 declare -a Environments=("BreakoutDeterministic-v4" "AsterixDeterministic-v4" "QbertDeterministic-v4" "SeaquestDeterministic-v4" "AsteroidsDeterministic-v4" )
 declare -a Expert=(1 5 30 )
 
@@ -7,7 +7,8 @@ declare -a Expert=(1 5 30 )
 for val in ${Environments[@]}; do
 	for exp in ${Expert[@]}; do
 		echo $val $exp
-		sbatch --gres=gpu:1 --cpus-per-task=2 --account=rrg-dpmeger --mem=16G --time=23:55:00 ./run_expert_dqn.sh $val -1 0 "expert_data.pkl" $exp $special
+    echo $special
+		sbatch --gres=gpu:1 --cpus-per-task=2 --account=rrg-dpmeger --mem=16G --time=02:00:00 ./run_expert_dqn.sh $val -1 0 "expert_data.pkl" $exp $special
 	done
 done
 #sbatch --gres=gpu:1 --cpus-per-task=2 --account=rrg-dpmeger --mem=16G --time=23:55:00 
