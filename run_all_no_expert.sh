ccd61ab866c04a5ad0f4f4185dbe4a326a447eab
Xiru Zhu <xiruzhu@hans.cim.mcgill.ca>
2019-09-19 21:54:35 -0400

    run_script

--- run_all_no_expert.sh
+++ run_all_no_expert.sh
@@ -12,10 +12,16 @@ then
 	exit 1
 fi
 
-declare -a Environments=("BreakoutDeterministic-v4" "SeaquestDeterministic-v4" "MontezumaRevengeDeterministic-v4" "MsPacman-Deterministic-v4" "QbertDeterministic-v4" )
+#declare -a Environments=("BreakoutDeterministic-v4" "SeaquestDeterministic-v4" "MontezumaRevengeDeterministic-v4" "MsPacman-Deterministic-v4" "QbertDeterministic-v4" )
+#declare -a Environments=(SeaquestDeterministic-v4" "MsPacman-Deterministic-v4" "QbertDeterministic-v4" )
+#declare -a learning_rates=(0.001 0.0005 0.00025 0.0001 0.00005 0.000025 0.00001)
 
+declare -a Environments=("BreakoutDeterministic-v4" )
+declare -a learning_rates=(0.001 )
 # Iterate the string array using for loop
 for val in ${Environments[@]}; do
-    echo $val
-    sbatch --gres=gpu:1 --cpus-per-task=2 --account=rrg-dpmeger --mem=64G --time=23:55:00 ./run_script.sh $python_script $val -1 0 "expert_data.pkl" 1 $special $lr $coef
+    for lr in ${learning_rates[@]}; do
+        echo $val $lr
+        sbatch --gres=gpu:1 --cpus-per-task=2 --account=rrg-dpmeger --mem=64G --time=2:55:00 ./run_script.sh $python_script $val -1 0 "expert_data.pkl" 1 "$special_lr_$lr" $lr $coef
+    done
 done
