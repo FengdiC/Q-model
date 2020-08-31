ecefd21f5cf08b3ca375a689ab6ed02f3fff8966
Xiru Zhu <xiru.zhu@mail.mcgill>
2020-08-31 13:07:47 -0400

    latest

--- run_model.sh
+++ run_model.sh
@@ -10,5 +10,5 @@ echo "$env $algo $seed $max_frames $expert_priority_decay $tf_dir"
 module load cuda cudnn
 source "$tf_dir/bin/activate"
 
-python dqfd.py --agent=$algo --seed=$seed --env_id="$env-v4" --expert_file="human_$env-v4_1" --max_frames=$max_frames --expert_priority_decay=$expert_priority_decay
+python dqfd.py --agent=$algo --seed=$seed --env_id="$env-v4" --expert_file="human_$env-v4_1.pkl" --max_frames=$max_frames --expert_priority_decay=$expert_priority_decay
 
