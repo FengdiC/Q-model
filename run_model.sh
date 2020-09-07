b9bd5a16f78fb95b139f41d54aec5af97be14375
xiruzhu <xiruzhu@yahoo.com>
2020-09-07 01:27:42 -0400

    latets

--- run_model.sh
+++ run_model.sh
@@ -6,10 +6,11 @@ seed=$3
 max_frames=$4
 expert_priority_modifier=$5
 tf_dir=$6
-custom_id=$7
-echo "$env $algo $seed $max_frames $expert_priority_decay $tf_dir $custom_id"
+lr_rate=$7
+custom_id=$8
+echo "$env $algo $seed $max_frames $expert_priority_decay $tf_dir $lr_rate $custom_id"
 module load cuda cudnn
 source "$tf_dir/bin/activate"
 
-python dqfd.py --agent=$algo --seed=$seed --env_id="$env-v4" --expert_file="human_$env-v4_5.pkl" --max_frames=$max_frames --expert_priority_modifier=$expert_priority_modifier --custom_id=$custom_id
+python dqfd.py --agent=$algo --seed=$seed --env_id="$env-v4" --expert_file="human_$env-v4_5.pkl" --max_frames=$max_frames --expert_priority_modifier=$expert_priority_modifier --lr=$lr_rate --custom_id=$custom_id
 
