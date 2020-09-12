c01e4d7ec4d9aa7197b0805dbeb7f3030bcb4c0c
Sitao Luan <lst@cedar5.cedar.computecanada.ca>
2020-09-11 18:32:07 -0700

    latest

--- run_model.sh
+++ run_model.sh
@@ -12,5 +12,5 @@ echo "$env $algo $seed $max_frames $expert_priority_decay $tf_dir $lr_rate $cust
 module load cuda cudnn
 source "$tf_dir/bin/activate"
 
-python dqfd.py --agent=$algo --seed=$seed --env_id="$env-v4" --expert_file="human_$env-v4_5.pkl" --max_frames=$max_frames --expert_priority_modifier=$expert_priority_modifier --lr=$lr_rate --custom_id=$custom_id
+python dqfd.py --agent=$algo --seed=$seed --env_id="$env-v4" --expert_file="human_$env-v4_1.pkl" --max_frames=$max_frames --expert_priority_modifier=$expert_priority_modifier --lr=$lr_rate --custom_id=$custom_id
 
